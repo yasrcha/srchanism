416836896d0c3d11d087e54c9efd7734e529cf37
unknown <yasminnasrocha@gmail.com>
2023-06-08 12:20:11 -0300

    feat:RoomNameAtt

--- js/navPages.js
+++ js/navPages.js
@@ -1,5 +1,6 @@
 let rooms = document.querySelectorAll('.rooms')
 let buttons = document.querySelectorAll('.nav-button')
+let roomName = document.querySelectorAll('.room__name')
 
 for (let i = 0; i < rooms.length; i++) {
     const button = buttons[i];
@@ -7,6 +8,7 @@ for (let i = 0; i < rooms.length; i++) {
 
     button.onclick = function () {
         removeClass()
+        attRoomName(room)
         room.classList.toggle('open-rooms')
     }
 }
@@ -20,4 +22,11 @@ function removeClass() {
             room.classList.remove('open-rooms')
         }
     }
+}
+
+function attRoomName(e) {
+    let name = e.classList[1]
+
+    roomName[0].innerHTML = `${name}`
+    roomName[1].innerHTML = `# ${name}`
 }
