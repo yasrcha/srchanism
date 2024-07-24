6e9bdb6629ebae9078170d42507dca7a573178cb
Yasmin R. <yasminnasdarocha@gmail.com>
2024-07-23 23:19:37 -0300

    style: Modificação Index Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import faymayPhoto from './assets/faymay.jpeg'
 import './App.css'
 
 function App() {
@@ -9,24 +8,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="http://x.com/srchanism" target="_blank">
+          <img src={faymayPhoto} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Srchanism Site/Card</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          O site está em construção, volte futuramente.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Progresso depende unicamente da boa vontade da @srchanism em estudar/codar.
       </p>
     </>
   )
